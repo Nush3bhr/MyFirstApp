9ab5baa9524efdd505764b5194286c39d027d731
Anushka <nush3bhr@gmail.com>
2020-12-17 00:05:33 +0530

    prettified

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,9 @@
 import React from "react";
-const Header = () =>{
-    return(
-        <div>
-            <h1>
-                My Header
-            </h1>
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div>
+      <h1>My Header page</h1>
+    </div>
+  );
+};
 export default Header;
