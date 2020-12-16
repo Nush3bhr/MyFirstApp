821f83d025ca8ffd2af96112276cbcfee6515692
Anushka <nush3bhr@gmail.com>
2020-12-16 23:36:05 +0530

    pretty formatted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from "react";
 //import Button from '@material-ui/core/Button';
 //import Demo from './Components/Demo';
-import Tweet from './Components/Tweet';
-import Header from './Header';
-import Nav from './Nav';
-import About from './About';
-import Shop from './Shop';
-import Footer from './Footer';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route}from 'react-router-dom';
-
+import Tweet from "./Components/Tweet";
+import Header from "./Header";
+import Nav from "./Nav";
+import About from "./About";
+import Shop from "./Shop";
+import Footer from "./Footer";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-     
-      <Router>
-      <div> 
-        <Nav/>
+  return (
+    <Router>
+      <div>
+        <Nav />
         <Route path="/about" component={About} />
-        <Route path="/shop" component={Shop} />      
-        <Tweet name= "Hey Abhishek!" message="Welcome You"/> 
-      
+        <Route path="/shop" component={Shop} />
+        <Tweet name="Hey Abhishek!" message="Welcome You" />
       </div>
-      </Router>
-    );
-  }
+    </Router>
+  );
+}
 
-export default App; 
+export default App;
